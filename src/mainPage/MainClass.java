ba484321f25c5fd56a51db595829bbd7e9b55eeb
yhj <tntj123@naver.com>
2021-08-27 21:34:24 +0900

    프로젝트 업로드

---
+++ src/mainPage/MainClass.java
@@ -0,0 +1,29 @@
+package mainPage;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		/*
+		 * 원하는 기능 만들어서 넣기
+		 */
+		Scanner scan = new Scanner(System.in);
+		int sel;
+		System.out.println("기능 선택");
+		System.out.println("1.조은별 2.한영수 3.김기범 4.김진수 5.유현준");
+		sel = scan.nextInt();
+		switch (sel) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		}
+
+	}
+}
