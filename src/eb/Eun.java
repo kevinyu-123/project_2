4798ca4c9504cc4eab29447421890cc2859523f4
eun <eb3020@naver.com>
2021-08-28 18:44:39 +0900

    은별기능추가

--- src/eb/Eun.java
+++ src/eb/Eun.java
@@ -1,19 +1,14 @@
-package mainPage;
+package eb;
 
 
 import java.util.Scanner;
 
 //id와passwd가 일치하면 로그인하고 로그아웃
-interface i {
-	public void login();
 
-	public void logout();
-}
 
-class Eun implements i {
-	String id;
-	String pw;
-
+public class Eun implements Jo {
+	public String id;
+	public String pw;
 	@Override
 	public void login() {
 		System.out.println("로그인 되었습니다.");
